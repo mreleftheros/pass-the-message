c16d1e3f25d03d8f06929818e0ab0fbee63cd95e
eleftheros <eleftheros_all@protonmail.com>
2021-08-12 15:02:13 +0300

    Add variables and event in JavaScript

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+const messageForm = document.getElementById("messageForm");
+const msg = document.getElementById("msg");
+const output = document.getElementById("output");
+
+
+// events
+messageForm.addEventListener("submit", submitMessage);
