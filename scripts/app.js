023ba796800de75e39bccde673d0b5f3be00ecd6
eleftheros <eleftheros_all@protonmail.com>
2021-08-12 15:09:23 +0300

    Add updateMessageUI function

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,25 @@ const messageForm = document.getElementById("messageForm");
 const msg = document.getElementById("msg");
 const output = document.getElementById("output");
 
+// function that submits the form
 const submitMessageForm = e => {
   e.preventDefault(); // prevent default form submit
   let value = e.target.message.value;
   
   if (value.length === 0) return; // simple check
 
+  updateMessageUI(value);
+
   e.target.reset(); // reset form fields
+};
+
+const updateMessageUI = value => {
+  output.innerHTML = `<strong>${value}</strong>`;
+  msg.innerHTML = `<em>Last Message Delivered</em>`;
+
+  setTimeout(() => {
+    msg.innerHTML = "";
+  },1500)
 };
 
 // events
