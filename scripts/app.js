985c70420aa308801dfa52dd068cf935c70f1a30
eleftheros <eleftheros_all@protonmail.com>
2021-08-12 15:05:45 +0300

    Add submitMessageForm function

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,14 @@ const messageForm = document.getElementById("messageForm");
 const msg = document.getElementById("msg");
 const output = document.getElementById("output");
 
+const submitMessageForm = e => {
+  e.preventDefault(); // prevent default form submit
+  let value = e.target.message.value;
+  
+  if (value.length === 0) return; // simple check
+
+  e.target.reset(); // reset form fields
+};
 
 // events
-messageForm.addEventListener("submit", submitMessage);
+messageForm.addEventListener("submit", submitMessageForm);
